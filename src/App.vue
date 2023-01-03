c75038357561ed1231c5d72cbac6a7b28656925a
priyanshumaurya8868 <priyanshumaurya.8868@gmail.com>
2023-01-03 18:45:21 +0530

    initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,17 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+<div>Hello {{name}}!</div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+ data(){
+ return {
+ name : "Priyanshu"
+ }
+ }
+
 }
 </script>
 
